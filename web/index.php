ea063148327293eb4ee7d2c9e9ffe5bd862a7f9e
Emily Fox <emily@pmg.com>
2015-05-04 21:43:03 -0500

    adding directories

---
+++ web/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Silex Shell
+ *
+ * Main entry point.
+ *
+ * @since       1.0
+ * @author      Emily Fox <emily@pmg.com>
+ * @copyright   2015 PMG
+ */
+
+require __DIR__ . '/../app/bootstrap.php';
+
+AppKernel::web();
